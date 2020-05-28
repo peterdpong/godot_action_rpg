5aef8a6e913fcd112c6cfd3f741de564d1a6a734
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2020-05-27 23:31:31 -0400

    Added cliff tileset with collisions

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,14 +10,18 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
+	# var mousePosition = get_local_mouse_position()
+	# mousePosition = mousePosition.normalized()
+	# print(mousePosition)
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	animationTree.set("parameters/Idle/blend_position", input_vector)
+	animationTree.set("parameters/Run/blend_position", input_vector)
 	
 	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
+		
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
