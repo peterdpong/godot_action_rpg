2807384748de27989317002ca00ede931ed27ce2
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2020-05-28 22:27:03 -0400

    Added signals and grass/grass destruction instancing

---
+++ World/Grass.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://Effects/GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		
+		#Add grassEffect instance
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
+		
+		queue_free()
