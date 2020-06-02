f0e86afce9105769dc38f8418adf7a98d7ff9fd4
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2020-06-01 23:21:30 -0400

    Added stats and damage

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -4,11 +4,20 @@ var knockback = Vector2.ZERO
 var knockback_frict = 200
 var knockback_speed = 125
 
+onready var stats = $Stats
+
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_frict * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	var knockback_direction = get_node("Hurtbox").global_position - area.global_position
+	stats.health -= area.damage
+	
+	var knockback_direction = get_node("Hurtbox").global_position - area.get_parent().global_position
 	knockback_direction = knockback_direction.normalized()
 	knockback = knockback_direction * knockback_speed
+
+
+func _on_Stats_no_health():
+	queue_free()
